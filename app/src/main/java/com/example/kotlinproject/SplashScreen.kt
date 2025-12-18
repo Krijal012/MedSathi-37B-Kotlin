68c5ef47d2112198781616dc2557e89161b99766
krijal7@gmail.com <krijalshrestha7@gmail.com>
2025-12-18 13:01:01 +0545

    added splash screen

---
+++ app/src/main/java/com/example/kotlinproject/SplashScreen.kt
@@ -0,0 +1,84 @@
+package com.example.kotlinproject
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.kotlinproject.ui.theme.KotlinProjectTheme
+import kotlinx.coroutines.delay
+
+class SplashScreen : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            KotlinProjectTheme {
+                Splash()
+            }
+        }
+    }
+}
+
+@Composable
+fun Splash(){
+    val context = LocalContext.current
+    val activity = context as Activity
+
+    LaunchedEffect(Unit) {
+        delay(3000)
+        val intent = Intent(context, LoginActivity::class.java)
+        context.startActivity(intent)
+        activity.finish()
+    }
+
+    Scaffold { padding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.White)
+                .padding(padding),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Image(
+                painter = painterResource(R.drawable.logo),
+                contentDescription = null,
+                modifier = Modifier.size(200.dp)
+            )
+            Spacer(
+                modifier = Modifier.height(80.dp)
+            )
+            CircularProgressIndicator(color = Color.Blue)
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SplashPreview() {
+    KotlinProjectTheme {
+        Splash()
+    }
+}
